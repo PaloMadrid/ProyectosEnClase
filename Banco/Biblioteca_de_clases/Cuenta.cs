942093fdd27c65d898ae12cfcf1e1c734d246b2b
PaloMadrid <paloma.madrid@bue.edu.ar>
2024-08-07 12:22:02 -0300

    commit

---
+++ Banco/Biblioteca_de_clases/Cuenta.cs
@@ -0,0 +1,25 @@
+﻿namespace Biblioteca_de_clases
+{
+    public class Cuenta
+    {
+        private int dni;
+        private string alias;
+        private double saldo;
+        private string tipoDeCuenta;
+        private int cbu;
+
+        public Cuenta(int dni, string alias, double saldo, string tipoDeCuenta, int cbu)
+        {
+            this.dni = dni;
+            this.alias = alias;
+            this.saldo = saldo;
+            this.tipoDeCuenta = tipoDeCuenta;
+            this.cbu = cbu;
+        }
+
+        public string MostrarInformacion()
+        {
+            return "DNI cliente: " + this.dni + " - Alias: " + this.alias + " - Tipo de cuenta: " + this.tipoDeCuenta + " - Saldo: " + this.saldo+ " - CBU: "+this.cbu;
+        }
+    }
+}
---
+++ Banco/testeo_banco/Program.cs
@@ -0,0 +1,39 @@
+﻿using Biblioteca_de_clases;
+
+namespace testeo_banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta cuenta1;
+           
+            Cuenta cuenta2 = new Cuenta(3698745,"juan",50000,"Cuenta corriente",657985465);
+            int dni;
+            string alias;
+            double saldo;
+            string tipoDeCuenta;
+            int cbu;
+
+
+            Console.WriteLine("ingrese su dni:");
+            dni=int.Parse( Console.ReadLine());
+
+            Console.WriteLine("Alias:");
+            alias = Console.ReadLine();
+
+            Console.WriteLine("saldo:");
+            saldo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("tpo de cuenta:");
+            tipoDeCuenta = (Console.ReadLine());
+
+            Console.WriteLine("cbu:");
+            cbu = int.Parse(Console.ReadLine());
+
+            cuenta1 = new Cuenta(dni, alias, saldo, tipoDeCuenta, cbu);
+
+            Console.WriteLine(cuenta1.MostrarInformacion());
+        }
+    }
+}
